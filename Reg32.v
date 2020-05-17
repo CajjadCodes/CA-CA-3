734b49a7052b2645e470415fc95537015510a8c0
CajjadCodes <62202221+CajjadCodes@users.noreply.github.com>
2020-05-17 16:51:08 +0430

    Adding LoadEnable to Reg32

--- Reg32.v
+++ Reg32.v
@@ -1,12 +1,13 @@
-module Reg32(clk, rst, d, q);
+module Reg32(clk, rst, d, ld, q);
 input clk;
 input rst;
 input [31:0] d;
+input ld;
 output reg [31:0] q;
 	
 	always @(posedge clk or posedge rst) begin
 		if (rst) q <= 32'b0;
-		else q <= d;
+		else if (ld) q <= d;
 	end
 
 endmodule 
